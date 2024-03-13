becad7a29a8dda09821d78e59b27af27c34daf74
Shimon Yzhaki <mct1957mct@gmail.com>
2024-03-13 17:08:53 +0000

    Dummy Version project

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -12,7 +12,19 @@ sap.ui.define([
 
             },
 
-            onSelectData: function(oEvent) {
+            onSelectedData: function(oEvent) {
+                debugger;
+                //let sServiceName = oEvent.getSource().getDataset().getBinding("data").getPath();
+                var index = oEvent.mParameters.data[0].data._context_row_number;
+                var sEntityKey = oEvent.getSource().getDataset().getBinding("data").aKeys[index];
+                var oBinding = oEvent.getSource().getDataset().getBinding("data").getModel().getProperty("/"+sEntityKey);
+                if( oBinding.ObjIdsStr && oBinding.ProcessTypeStr){
+                    const url = `${this.crmUrl}?objecttype=${oBinding.Objtype}&processtype=${oBinding.ProcessTypeStr}&objidstring=${oBinding.ObjIdsStr}`;
+                    // var url = this.crmUrl + "?objecttype=LEAD&processtype=" + oBinding.ProcessTypeStr + "&objidstring=" + oBinding.ObjIdsStr;
+                    // if(client)
+                        // url+="&" + client;
+                    window.open(url,"_blank");
+                }
                 debugger;
             }
         });
